b0e0f371f2c8b7a4f3d3179be47d2cf31e7aaf45
FallenAngel7 <mohammad.mdi777@gmail.com>
2018-11-12 14:05:04 +0330

    Initial commit for starting a huge dashboard

--- cfzDashboard/Program.cs
+++ cfzDashboard/Program.cs
@@ -16,7 +16,7 @@ namespace cfzDashboard
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
